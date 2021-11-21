e37ab94ae612328fed1fe8c15ed832b82eb9da74
“VenuReddy773” <venu706@gmail.com>
2021-11-21 20:59:07 +0530

    [VenuGopalReddy] Added: Reverse LinkedList Program

--- DataStructures/DataStructures/Program.cs
+++ DataStructures/DataStructures/Program.cs
@@ -11,7 +11,7 @@ namespace DataStructures
             bool flag = true;
             while (flag)
             {
-                Console.WriteLine("Choose an Option : \n 1.Create Simple Linked List \n 2.Exit\n");
+                Console.WriteLine("Choose an Option : \n 1.Create Simple Linked List \n 2.AddInReverse\n3.Exit\n");
                 int option = Convert.ToInt32(Console.ReadLine());
                 switch (option)
                 {
@@ -21,6 +21,12 @@ namespace DataStructures
                         linkedList.Add(70);
                         break;
                     case 2:
+                        linkedList.AddInReverseOrder(70);
+                        linkedList.AddInReverseOrder(30);
+                        linkedList.AddInReverseOrder(56);
+                        linkedList.Display();
+                        break;
+                    case 3:
                         flag = false;
                         break;
                 }
